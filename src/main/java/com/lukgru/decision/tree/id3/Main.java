946b65f857543589c60f951b403b4ee034292d93
LukaszGruba <lukgru@gmail.com>
2017-02-07 20:57:53 +0100

    init

---
+++ src/main/java/com/lukgru/decision/tree/id3/Main.java
@@ -0,0 +1,11 @@
+package com.lukgru.decision.tree.id3;
+
+/**
+ * Created by Łukasz on 2017-02-07.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO: implement
+    }
+}

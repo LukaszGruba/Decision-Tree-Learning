8acd648f56cc8eaddfa67e33b54ae4cbba4bfa59
LukaszGruba <lukgru@gmail.com>
2017-02-11 23:15:44 +0100

    Value equals + hashcode

--- src/main/java/com/lukgru/decision/tree/id3/data/Value.java
+++ src/main/java/com/lukgru/decision/tree/id3/data/Value.java
@@ -14,4 +14,24 @@ public class Value {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Value value1 = (Value) o;
+
+        return value != null ? value.equals(value1.value) : value1.value == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
 }
